033cf72492c056eda914cb8ec46795f65595cd40
Dimitris-Provatas <james.provatas@gmail.com>
2021-03-15 15:43:31 +0200

    single days and weeks are done, serialization of data to store in localstorage is done, retrieval of such data is done, arrows now function properly and have the correct constrains
    TODO: month and year setups, fix a bug where the last day of the month is not showing up properly and breaks the rest of the week

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+type directions = "left" | "both" | "right";
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -41,11 +43,15 @@ export class MainComponent implements OnInit
 
   loading: boolean = false;
 
+  allowedDirections: directions = "both";
   targetType: string = "week";
+
   today: Date;
+  targetDay: Date;
   todayToString: string = "";
+
   weekNumber: number;
-  thisWeek: Date[] = [];
+  targetWeek: Date[] = [];
 
   allNotes: any[][][] = [];
 
@@ -55,23 +61,22 @@ export class MainComponent implements OnInit
   ngOnInit(): void
   {
     this.today = new Date();
+    this.targetDay = new Date();
     this.setup(this.today);
+    this.allNotes = this.allNoteSetup();
   }
 
   setup(day: Date): void
   {
     this.todayToString = this.dateToString(day);
-    this.weekNumber = this.getWeekNumber(day);
-    this.thisWeek = this.setupWeek(day);
-
-    this.allNotes = this.allNoteSetup();
-    // import from storage
+    this.targetWeek = this.setupWeek(day);
   }
 
   allNoteSetup(): any[][][]
   {
     var toReturn: any[][][] = [];
 
+    // populate the array with empty cells
     for (let y = 0; y < 3; y++)
     {
       toReturn.push([]);
@@ -88,7 +93,38 @@ export class MainComponent implements OnInit
       }
     }
 
+    // get existing localStorage entries' keys
+    const keys = Object.keys(localStorage);
+
+    // get the values of each key and add them to the correct cells
+    keys.forEach(async key =>
+    {
+      const retrieved = await localStorage.getItem(key)
+      const keySplit = key.split("/");
+      toReturn[parseInt(keySplit[2]) - this.today.getFullYear() + 1][parseInt(keySplit[1]) - 1][parseInt(keySplit[0])] = JSON.parse(retrieved);
+    });
+
     return toReturn;
+  }
+
+  async saveFunction(day: Date): Promise<void>
+  {
+    this.loading = true;
+
+    const targetDay = this.dateToString(day);
+
+    await localStorage.setItem(targetDay, JSON.stringify(this.allNotes[day.getFullYear() - this.today.getFullYear() + 1][day.getMonth()][day.getDate()]));
+
+    this.loading = false;
+  }
+
+  getWeekNumber(day: Date): number
+  {
+    const targetDay = Date.UTC(day.getFullYear(), day.getMonth(), day.getDate() - 3);
+    const yearStart = Date.UTC(day.getFullYear(), 0, 1);
+    const diff = (targetDay - yearStart);
+
+    return Math.ceil(((diff / 86400000) + 1) / 7);
   }
 
   daysInMonth(month, year)
@@ -98,37 +134,17 @@ export class MainComponent implements OnInit
 
   addNoteToDay(day: Date): void
   {
-    console.log(day);
-    // make targetNotes equal to target day
-    this.allNotes[day.getFullYear() - this.today.getFullYear()][day.getMonth()][day.getDate()].push("");
+    this.allNotes[day.getFullYear() - this.today.getFullYear() + 1][day.getMonth()][day.getDate()].push("");
+    this.saveFunction(day);
   }
 
   removeNoteFromDay(day: Date, index: number): void
   {
     if (confirm("Are you sure? You cannot revert this action!"))
-      this.allNotes[day.getFullYear()][day.getMonth()][day.getDate()].splice(index, 1);
-  }
-
-  saveFunction(day: Date): void
-  {
-    this.loading = true;
-
-    const targetDay = this.dateToString(day);
-    const retrieved = localStorage.getItem(targetDay);
-
-    console.log(`Saving for ${day}`);
-    // save array of notes to memory
-
-    // first check if there was an entry before and no items to save currently
-    if (retrieved !== null && retrieved !== undefined && this.allNotes[day.getFullYear()][day.getMonth()][day.getDate()].length === 0)
     {
-      localStorage.removeItem(targetDay);
-      return;
+      this.allNotes[day.getFullYear() - this.today.getFullYear() + 1][day.getMonth()][day.getDate()].splice(index, 1);
+      this.saveFunction(day);
     }
-
-    localStorage.setItem(targetDay, JSON.stringify(this.allNotes[day.getFullYear()][day.getMonth()][day.getDate()]));
-
-    this.loading = false;
   }
 
   capitalizeFirstLetter(word: string): string
@@ -143,15 +159,6 @@ export class MainComponent implements OnInit
     return value;
   }
 
-  getWeekNumber(day: Date): number
-  {
-    const targetDay = Date.UTC(day.getFullYear(), day.getMonth(), day.getDate() - 3);
-    const yearStart = Date.UTC(day.getFullYear(), 0, 1);
-    const diff = (targetDay - yearStart);
-
-    return Math.ceil(((diff / 86400000) + 1) / 7);
-  }
-
   setupWeek(day: Date): Date[]
   {
     var toReturn: Date[] = [];
@@ -164,6 +171,8 @@ export class MainComponent implements OnInit
       toReturn.push(new Date(weekStart));
       weekStart.setDate(weekStart.getDate() +1);
     }
+
+    this.weekNumber = this.getWeekNumber(day);
 
     return toReturn;
   }
@@ -182,5 +191,111 @@ export class MainComponent implements OnInit
   trackByFn(index: any, item: any): any
   {
     return index;
+  }
+
+  arrow(context: string, dir: string): void
+  {
+    // ignore if chosen direction is not allowed
+    if (dir === "left" && this.allowedDirections === "right") return;
+    if (dir === "right" && this.allowedDirections === "left") return;
+
+    this.allowedDirections = "both";
+
+    switch (context)
+    {
+      case "day":
+        // check if it is the first day of the previous year or the last day of the next year
+        // then change the allowedDirections as necassery
+        switch (dir)
+        {
+          case "left":
+            this.targetDay.setDate(this.targetDay.getDate() - 1);
+            if (this.targetDay.getFullYear() < this.today.getFullYear() - 1)
+            {
+              this.targetDay.setDate(this.targetDay.getDate() + 1);
+              this.allowedDirections = 'right';
+            }
+            break;
+          case "right":
+            this.targetDay.setDate(this.targetDay.getDate() + 1);
+            if (this.targetDay.getFullYear() > this.today.getFullYear() + 1)
+            {
+              this.targetDay.setDate(this.targetDay.getDate() - 1);
+              this.allowedDirections = 'left';
+            }
+            break;
+        }
+        break;
+      case "week":
+        // check if it is the first wekk of the previous year or the last week of the next year
+        // then change the allowedDirections as necassery
+        switch (dir)
+        {
+          case "left":
+            this.targetDay.setDate(this.targetDay.getDate() - 7);
+            if (this.targetDay.getFullYear() < this.today.getFullYear() - 1)
+            {
+              this.targetDay.setDate(this.targetDay.getDate() + 7);
+              this.allowedDirections = 'right';
+            }
+            break;
+          case "right":
+            this.targetDay.setDate(this.targetDay.getDate() + 7);
+            if (this.targetDay.getFullYear() > this.today.getFullYear() + 1)
+            {
+              this.targetDay.setDate(this.targetDay.getDate() - 7);
+              this.allowedDirections = 'left';
+            }
+            break;
+        }
+        this.targetWeek = this.setupWeek(this.targetDay);
+        break;
+      case "month":
+        // check if it is the first month of the previous year or the last month of the next year
+        // then change the allowedDirections as necassery
+        switch (dir)
+        {
+          case "left":
+            this.targetDay.setMonth(this.targetDay.getMonth() - 1);
+            if (this.targetDay.getFullYear() < this.today.getFullYear() - 1)
+            {
+              this.targetDay.setMonth(this.targetDay.getMonth() + 1);
+              this.allowedDirections = 'right';
+            }
+            break;
+          case "right":
+            this.targetDay.setMonth(this.targetDay.getMonth() + 1);
+            if (this.targetDay.getFullYear() > this.today.getFullYear() + 1)
+            {
+              this.targetDay.setMonth(this.targetDay.getMonth() - 1);
+              this.allowedDirections = 'left';
+            }
+            break;
+        }
+        break;
+      case "year":
+        // check if it is the last year or the next year
+        // then change the allowedDirections as necassery
+        switch (dir)
+        {
+          case "left":
+            this.targetDay.setFullYear(this.targetDay.getFullYear() - 1);
+            if (this.targetDay.getFullYear() < this.today.getFullYear() - 1)
+            {
+              this.targetDay.setFullYear(this.targetDay.getFullYear() + 1);
+              this.allowedDirections = 'right';
+            }
+            break;
+          case "right":
+            this.targetDay.setFullYear(this.targetDay.getFullYear() + 1);
+            if (this.targetDay.getFullYear() > this.today.getFullYear() + 1)
+            {
+              this.targetDay.setFullYear(this.targetDay.getFullYear() - 1);
+              this.allowedDirections = 'left';
+            }
+            break;
+        }
+        break;
+    }
   }
 }
